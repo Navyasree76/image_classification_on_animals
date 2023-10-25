c1124f2fb6d9de76d5c4f455049ab06cf3ca6927
Navyasree76 <134192333+Navyasree76@users.noreply.github.com>
2023-10-25 10:51:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class_labels = {
 }
 
 # Specify the path to your custom model
-model_path = r"C:\Users\sandeep\OneDrive\Desktop\Navya_files\Mini Project\mobilenet_v2.h5"  # Replace with the actual path to your model file
+model_path = mobilenet_v2.h5  # Replace with the actual path to your model file
 
 # Load the custom model
 model = MobileNetV2(weights=None)
